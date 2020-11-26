95f4f4324040be184db37d673654d2b0eee03194
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 16:04:30 +0100

    aded bunch of stuff

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 //import path module
 const path = require("path")
+
 //import express
 const express = require('express')
+
 //initialized express
 const app = express()
+
+//this will ensure the start
+app.use(express.urlencoded({ extended: false }))
+
+//convert or pare incoming json data
+app.use(express.json())
+
+//gain acces to everything inside of public /you do not need to declare path to files manually like prevously
+app.use(express.static("public"))
+
+//views are the files we render in the webbrowser (we need to install a viewengies)
+app.set("views", "views")
 
 
 //setting up routes
