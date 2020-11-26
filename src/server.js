2f47812d996c6ba14ba70f15429a3ff4a0185dd6
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 15:54:25 +0100

    better mode

--- src/server.js
+++ src/server.js
@@ -7,30 +7,13 @@ const app = express()
 
 
 //setting up routes
-/*
 app.get("/", (req, res) => {
     res.send("My first express Web page")
 })
-*/
 
-app.get("/", (req, res) => {
-    res.sendFile("index.html", {
-        root: path.join(__dirname, "../public/")
-    })
-})
-
-app.get("/about", (req, res) => {
-    res.sendFile("about.html", {
-        root: path.join(__dirname, "../public/")
-    })
-})
-
-
-/*
 app.get("/about", (req, res) => {
     res.send("the about page")
 })
-*/
 
 app.listen(3000, () => {
     console.log("The server is now running on port 3000")
