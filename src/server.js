08b7b18f00caf1af3781111e3573adaccca9901d
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 15:32:32 +0100

    added fikses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,14 @@ const express = require('express')
 //initialized express
 const app = express()
 
-//
+//setting up routes
 app.get("/", (req, res) => {
     res.send("My first express Web page")
 })
+
+app.get("/about", (req, res) => {
+    res.send("the about page")
+})
+
 
 app.listen(3000)
