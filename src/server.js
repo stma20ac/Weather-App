bb905114115e8b3d749dce4a025d79aa900aa6bf
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 15:51:11 +0100

    added server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,37 @@
+//import path module
+const path = require("path")
 //import express
 const express = require('express')
-
 //initialized express
 const app = express()
 
+
 //setting up routes
+/*
 app.get("/", (req, res) => {
     res.send("My first express Web page")
 })
+*/
 
+app.get("/", (req, res) => {
+    res.sendFile("index.html", {
+        root: path.join(__dirname, "../public/")
+    })
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile("about.html", {
+        root: path.join(__dirname, "../public/")
+    })
+})
+
+
+
+/*
 app.get("/about", (req, res) => {
     res.send("the about page")
 })
+*/
 
 app.listen(3000, () => {
     console.log("The server is now running on port 3000")
