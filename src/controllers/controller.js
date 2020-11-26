f7452a585ff3c225b5d75ebff8ca29777af1c922
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 18:18:25 +0100

    added axios

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,9 +1,13 @@
+const axios = require("axios")
+
+const API_KEY = "0a4419dff48c24393f06fcf8d586d124"
+
 exports.renderHomePage = (req, res) => {
     res.render("index")
 }
 
 exports.getWeather = (req, res) => {
-    res.send(`you typed ${req.body.city}.`)
+    const city = req.body.city
 }
 
 exports.renderAboutPage = (req, res) => {
