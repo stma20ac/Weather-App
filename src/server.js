b09ae761be88ab0ce527284bc4fd7bcfa7253300
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 15:24:52 +0100

    added comments

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 //import express
 const express = require('express')
 
+//initialized express
 const app = express()
 
+//
 app.get("/", (req, res) => {
     res.send("My first express Web page")
 })
