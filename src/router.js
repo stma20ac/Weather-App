1d69b88cd8fcca97ce93606f4d245e4fc7e196a8
Steffan Maksimovic <stefanovic@MacBook-Pro-tilhrende-Steffan.local>
2020-11-26 17:45:05 +0100

    added weather app

--- src/router.js
+++ src/router.js
@@ -1,15 +1,10 @@
 const express = require('express') 
 const router = express.Router()
+const controller = require("./controllers/controller")
 
 //setting up routes
-router.get("/", (req, res) => {
-    res.render("index", {
-        title: "Sign up"
-    })
-})
+router.get("/", controller.renderHomePage)
 
-router.get("/about", (req, res) => {
-    res.render("about")
-})
+router.get("/about", controller.renderAboutPage)
 
 module.exports = router
